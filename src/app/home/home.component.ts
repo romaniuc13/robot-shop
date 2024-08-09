cfac30c0df7997464c84ecae6b5d4f7a2d291287
OPENLM-NET\maximr <maxim.romaniuc@openlm.com>
2024-08-09 14:32:06 +0300

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,4 @@
-import { Component } from '@angular/core';
-
 @Component({
   selector: 'bot-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
